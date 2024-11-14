892340a67a0ec642b3589f8600ebf2264e375705
Manu <manuelenriquesantosiglesias@gmail.com>
2024-11-14 20:21:31 +0100

    Cambios para ejecución como root, cambio de espacio por enter, más checks

--- script.sh
+++ script.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+
+# Check root
+if [[ $EUID -ne 0 ]]; then
+    echo "Este script necesita permisos de superusuario. Ejecútalo con 'sudo'."
+    exit 1
+fi
+
+# User Name
+USER_HOME=$(logname)
+
 echo "Por favor, monta las Guest Additions en VirtualBox."
 echo "Pulsa espacio para continuar una vez montadas..."
 
-# Wait spacebar
-while :; do
-    read -n1 -r key
-    if [[ $key == ' ' ]]; then
-        break
-    fi
-done
+# Wait enter
+read -r -p "Presiona Enter para continuar..."
 
 # Check Guest additions are mounted
-GUEST_ADDITIONS_DIR=$(find /media/$USER -type d -name "VBox_GAs_*" 2>/dev/null)
+GUEST_ADDITIONS_DIR=$(ls -d /media/$USER_HOME/VBox_GAs_* 2>/dev/null | tail -n 1)
 
 if [[ -d "$GUEST_ADDITIONS_DIR" ]]; then
     echo "Guest Additions detectadas en $GUEST_ADDITIONS_DIR. Continuando con la instalación..."
@@ -19,6 +24,6 @@ if [[ -d "$GUEST_ADDITIONS_DIR" ]]; then
     cd "$GUEST_ADDITIONS_DIR" || exit
     sudo ./VBoxLinuxAdditions.run
 else
-    echo "No se detectan las Guest Additions en el directorio /media/$USER/. Por favor, verifica que estén montadas."
+    echo "No se detectan las Guest Additions en el directorio /media/$USER_HOME/. Por favor, verifica que estén montadas."
     exit 1
 fi
